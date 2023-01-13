1127ebf053d4497fda091ac2d23b9f69de63bae3
hucksy <hucksy@tutanota.com>
2023-01-12 18:21:25 -0700

    first attempt at waqi etl

--- services/etl/dags/config.py
+++ services/etl/dags/config.py
@@ -1,17 +1,12 @@
-from functools import lru_cache
-
 from pydantic import BaseSettings
 
 
-class EnvSettings(BaseSettings):
+class Settings(BaseSettings):
+    """config for airflow"""
     POSTGRES_URI = "postgresql://airflow:airflow@postgres:5432/air_quality"
     WAQI_BASE_URL = "https://api.waqi.info/"
     WAQI_TOKEN = "bb10d851797e497b46823a5b4f984354c6ff4d9a"
 
     class Config:
-        env_file = '.env'
+        env_file = '/opt/airflow/dags/.env.airflow'
         env_file_encoding = 'utf8'
-
-@lru_cache()
-def get_env_configs():
-    return EnvSettings()
