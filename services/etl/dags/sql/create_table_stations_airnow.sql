f8503d5ce9d280a9f826ba27a98d076561c98331
myshkins <alex@ak0.io>
2023-02-02 16:23:30 -0500

    cleanup

--- services/etl/dags/sql/create_table_stations_airnow.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists stations_airnow(
-    station_name varchar not null primary key,
-    agency_name varchar,
-    latitude    numeric(10,6) not null,
-    longitude   numeric(10,6) not null,
-    location_coord geometry(point, 4326)
-);
