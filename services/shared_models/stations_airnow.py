dac09ff088d28cb5486a4a19673f8789621b35b1
hucksy <hucksy@tutanota.com>
2023-02-03 14:13:48 -0700

    alembic revision with station_id for airnow tables

--- services/shared_models/stations_airnow.py
+++ services/shared_models/stations_airnow.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Numeric, String
 from geoalchemy2 import Geometry
+from sqlalchemy import Column, Integer, Numeric, String
+
 from . import Base
 
 
 class Airnow_Stations(Base):
     __tablename__ = 'stations_airnow'
 
-    station_name = Column(String, primary_key=True, nullable=False)
+    station_id = Column(Integer, primary_key=True, nullable=False)
+    station_name = Column(String, nullable=False)
     agency_name = Column(String, nullable=False)
     latitude = Column(Numeric(10, 6), nullable=False)
     longitude = Column(Numeric(10, 6), nullable=False)
