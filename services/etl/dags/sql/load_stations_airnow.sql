410278590aa6a3a0da20dc8c5373956aeea34bfe
myshkins <alex@ak0.io>
2023-02-04 14:50:53 -0500

    airnow dags workin

--- services/etl/dags/sql/load_stations_airnow.sql
+++ services/etl/dags/sql/load_stations_airnow.sql
@@ -1,4 +1,5 @@
 insert into stations_airnow (
+    station_id,
     station_name,
     agency_name,
     latitude,
@@ -6,13 +7,15 @@ insert into stations_airnow (
     location_coord
     )
 select
+    station_id,
     station_name,
     agency_name,
     latitude,
     longitude,
     location_coord
     from stations_airnow_temp
-on conflict (station_name) do update set
+on conflict (station_id) do update set
+    station_id=excluded.station_id,
     station_name=excluded.station_name,
     agency_name=excluded.agency_name,
     latitude=excluded.latitude,
--- services/etl/dags/sql/trim_airnow_stations.sql
+++ services/etl/dags/sql/trim_airnow_stations.sql
@@ -1,7 +1,7 @@
 -- removes all stations not represented in airnow_readings
 delete from stations_airnow_temp 
-where station_name not in 
-(select distinct stations_airnow_temp.station_name 
+where station_id not in 
+(select distinct stations_airnow_temp.station_id 
 from stations_airnow_temp 
 join readings_airnow
-on stations_airnow_temp.station_name = readings_airnow.station_name);
+on stations_airnow_temp.station_id = readings_airnow.station_id);
