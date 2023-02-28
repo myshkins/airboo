5d5260ecf9c971efb41424055829e025e2c1bee5
myshkins <alex@ak0.io>
2023-02-28 11:27:07 -0500

    pytest in containers functioning ok

--- ci_cd/test/api/test_main.py
+++ ci_cd/test/api/test_main.py
@@ -1,6 +1,4 @@
-import pytest
 import requests
-from services.api.main import app
 
 
 def test_health_check():
@@ -9,6 +7,6 @@ def test_health_check():
     WHEN health check endpoint is called with GET method
     THEN response with status 200 and body OK is returned
     """
-    response = requests.get("localhost:8100/health-check")
+    response = requests.get("http://air_api:8100/health-check")
     assert response.status_code == 200
     assert response.json() == {"message": "OK"}
