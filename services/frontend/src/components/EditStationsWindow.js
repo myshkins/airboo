a5fe1a28fee1737d69c786d1fabc860860858172
myshkins <alex@ak0.io>
2023-03-03 17:46:57 -0500

    Fixed up css for EditStationWindow component
    Revise the css to use a grid layout. Remove unneccesary wrapping divs
    and just made it look nicer :)

--- services/frontend/src/components/EditStationsWindow.js
+++ services/frontend/src/components/EditStationsWindow.js
@@ -5,38 +5,34 @@ const EditStationsWindow = (props) => {
   return (
     <div className="edit-station-window">
       <h3>Add/Remove Stations</h3>
-      <div className={"add-remove-wrap"}>
-        <div className="add-remove-zip-entry">
-          <label htmlFor="zipcode-input">zipcode:</label>
-          <input
-            type="text"
-            name="zipcode-input"
-            value={props.zipcode}
-            onChange={props.handleZipcodeChange}
-          />
-        </div>
-        <HomeButton
-          value={"find stations"}
-          onClick={props.handleZipQueryChange}
+      <div className={"add-remove-zip-input-wrap"}>
+        <label htmlFor="zipcode-input">zipcode:</label>
+        <input
+          type="text"
+          name="zipcode-input"
+          value={props.zipcode}
+          onChange={props.handleZipcodeChange}
         />
       </div>
+      <HomeButton
+        value={"find stations"}
+        onClick={props.handleZipQueryChange}
+      />
       {props.tempStations != null ? <h5>select stations to include</h5> : null}
-      <div>
-        <form onSubmit={props.updateStations}>
-          {props.tempStations.map((station) => (
-            <AddStationCheckbox
-              key={station["station_id"]}
-              name={station["station_id"]}
-              pollutants={station["pollutants"]}
-              value={station["station_name"]}
-              checked={station["checked"]}
-              onChange={props.handleTempCheckChange}
-              type="checkbox"
-            />
-          ))}
-          <input type="submit" value="update stations" />
-        </form>
-      </div>
+      <form onSubmit={props.updateStations}>
+        {props.tempStations.map((station) => (
+          <AddStationCheckbox
+            key={station["station_id"]}
+            name={station["station_id"]}
+            pollutants={station["pollutants"]}
+            value={station["station_name"]}
+            checked={station["checked"]}
+            onChange={props.handleTempCheckChange}
+            type="checkbox"
+          />
+        ))}
+        <input type="submit" value="update stations" />
+      </form>
     </div>
   );
 };
