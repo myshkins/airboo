a607f7badff874ae3df73b84a2cd70c551f6638a
myshkins <alex@ak0.io>
2023-03-27 22:02:10 -0400

    Edit api startup.sh, docker-compose.yaml env file, favicon
    Edit the api startup.sh to separate alembic migrations from development
    mode. Change .env file for docker-compose, now using .env.dev. Fix
    dev_mode in PeriodEnum in pydantic_models.py. Add favicon and new cloud
    logo.

--- services/api/startup.sh
+++ services/api/startup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ $DEV_MODE == true ];then
+if [[ $DO_MIGRATIONS == 1 ]];
+then
+    echo DO_MIGRATIONS = $DO_MIGRATIONS 
     echo performing alembic migrations
     alembic upgrade head
 fi
 
-uvicorn main:app --host 0.0.0.0 --port 10100 --reload
+if [[ $DEV_MODE == 1 ]];
+then
+    echo DEV_MODE = $DEV_MODE
+    uvicorn main:app --host 0.0.0.0 --port 10100 --reload
+else
+    uvicorn main:app --host 0.0.0.0 --port 10100
+fi
